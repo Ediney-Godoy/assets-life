6e65a0578ba9b6614f91858b89414479d363c05a
Ediney-Godoy <ediney.dev@gmail.com>
2026-02-24 18:02:29 -0300

    FixBlankScreen

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 import { Routes, Route, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
